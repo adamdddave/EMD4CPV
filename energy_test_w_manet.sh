c5f22fe1a67cddb4fadbc580746dabb166bf7b15
Adam Davis <adamdddave@googlemail.com>
2023-01-30 16:19:03 +0000

    add some comments to the wrapper shell script

--- energy_test_w_manet.sh
+++ energy_test_w_manet.sh
@@ -1,3 +1,9 @@
-./Manet_noROOT -f1 ~/Wavelets/EMD/data/X_m2_temp.txt -f2 ~/Wavelets/EMD/data/Xbar_m2_temp.txt -s 0.4 -p 0
+#!/bin/bash
+echo "A simple wrapper which will run the MANET code on two locations. Consider setting in this script the variables LOCATION_OF_DATA and LOCATION_OF_OUTPUT"
+echo "./Manet_noROOT can be found from the get_manet_svn.sh script"
+LOCATION_OF_DATA=/my/path/to/data/
+LOCATION_OF_OUTPUT=/path/to/my/output/
 
-mv Ts.txt <path-to>/EMD/data/T_w_manet_CPV.txt
+./Manet_noROOT -f1 $LOCATION_OF_DATA/X_m2_temp.txt -f2 LOCATION_OF_DATA/Xbar_m2_temp.txt -s 0.4 -p 0
+
+mv Ts.txt $LOCATION_OF_OUTPUT/T_w_manet_CPV.txt
